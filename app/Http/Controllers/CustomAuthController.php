73a8c5a27aa01c96e4418ac11d6a9aba2e292163
Sreejith KR <sreejithkr@Sreejiths-MacBook-Air.local>
2023-10-30 16:38:10 +0530

    update

--- app/Http/Controllers/CustomAuthController.php
+++ app/Http/Controllers/CustomAuthController.php
@@ -43,22 +43,37 @@ class CustomAuthController extends Controller
             'password' => 'required|min:5|max:12',
         ]);
 
-        $user = User::where('email', $request->input('email'))->first();
+        $user = User::where('email','=', $request->email)->first();
 
         if($user) {
-            if (Hash::check($request->input('password'), $user->password)) {
+            if (Hash::check($request->password, $user->password)) {
 
+                $request->session()->put('loginId',$user->id);
                 return redirect('dashboard');
             } else {
                 return back()->with('fail', 'Incorrect password');
+                
             }
         } else {
             return back()->with('fail', 'This email is not registered.');
         }
     }
-public function dashboard(){
-    return  view ('dashboard');
+public function dashboard()
+{
+    $data = array();
+    if(Session::has('loginId')) {
+        $data = User::where('id', '=', Session::get('loginId'))->first();
+
+    }
+    return  view ('dashboard',compact('data'));
 }
 
+
+public function logout(){
+    if(Session::has('loginId')){
+        Session::pull('loginId');
+       return redirect('login');
+    }
+}
 
 }
--- resources/views/auth/dashboardmaster.blade.php
+++ resources/views/auth/dashboardmaster.blade.php
@@ -172,7 +172,7 @@
                         <div class="dropdown-menu dropdown-menu-end bg-light border-0 rounded-0 rounded-bottom m-0">
                             <a href="#" class="dropdown-item">My Profile</a>
                             <a href="#" class="dropdown-item">Settings</a>
-                            <a href="#" class="dropdown-item">Log Out</a>
+                            <a href="logout" class="dropdown-item">Log Out</a>
                         </div>
                     </div>
                 </div>
@@ -262,60 +262,24 @@
                             <thead>
                                 <tr class="text-dark">
                                     <th scope="col"><input class="form-check-input" type="checkbox"></th>
-                                    <th scope="col">Date</th>
-                                    <th scope="col">Invoice</th>
-                                    <th scope="col">Customer</th>
-                                    <th scope="col">Amount</th>
-                                    <th scope="col">Status</th>
-                                    <th scope="col">Action</th>
+                                    <th scope="col">ID</th>
+                                    <th scope="col">NAME</th>
+                                    <th scope="col">EMAIL</th>
+                                    
+                                   
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
                                     <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
+                                    <td>{{$data->id}}</td>
+                                    <td>{{$data->name}}</td>
+                                    <td>{{$data->email}}</td>
+                                   
+                                    
                                 </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                                </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                                </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                                </tr>
-                                <tr>
-                                    <td><input class="form-check-input" type="checkbox"></td>
-                                    <td>01 Jan 2045</td>
-                                    <td>INV-0123</td>
-                                    <td>Jhon Doe</td>
-                                    <td>$123</td>
-                                    <td>Paid</td>
-                                    <td><a class="btn btn-sm btn-primary" href="">Detail</a></td>
-                                </tr>
+                                
+                               
                             </tbody>
                         </table>
                     </div>
